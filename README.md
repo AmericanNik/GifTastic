f28b09069d50223831bc6e7c127f9338dba2e123
AmericanNik <49841405+AmericanNik@users.noreply.github.com>
2019-08-01 05:29:24 -0400

    put a little readme, got rid of some extra fluff, added button styling.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # GifTastic
+
+Welcome to the GifTastic App!
+
+Play Here: https://americannik.github.io/GifTastic/
+
+Dynamically generated buttons, add your own with the search input!
--- app.js
+++ app.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   userSubmit.on('click', function(event) {
     userGives = inputBox.val().trim()
-    topics.push(userGives)
+    topics.push(userGives);
     inputBox.val('');
     generateButtons();
   });
@@ -18,7 +18,7 @@ $(document).ready(function() {
   const generateButtons = function() {
     $('#buttonHolder').empty();
     topics.forEach(function(topic) {
-      let button = $('<button>').on('click', function() {
+      let button = $('<button>').addClass('btn btn-dark').on('click', function() {
         docDisplay.empty();
         let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=42Jbadnmd723gU0vIJSYwE3eeyXksAjT&limit=10&rating=g&q=" + topic;
         $.ajax({
@@ -26,8 +26,8 @@ $(document).ready(function() {
           method: "GET"
         }).then(function(response) {
           response.data.forEach(img => {
-            const image = $('<img />').attr('src', img.images.downsized_still.url).on('click', function() {
-              var src = ($(this).attr('src') === img.images.downsized_still.url)
+            let image = $('<img />').attr('src', img.images.downsized_still.url).on('click', function() {
+              let src = ($(this).attr('src') === img.images.downsized_still.url)
                ? img.images.original.url
                : img.images.downsized_still.url;
               $(this).attr('src', src);
