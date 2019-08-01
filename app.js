f0c5c92b41e2b70d336928b1caa3c7c42b4285cc
AmericanNik <49841405+AmericanNik@users.noreply.github.com>
2019-08-01 04:23:57 -0400

    images clickable that toggle through src attr to from a still image to playing.

--- app.js
+++ app.js
@@ -1,54 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function() {
   console.log('ready');
 
-let topics = ['murica','freedom','usa','patriot','america','donald trump', 'hillary clinton','barack obama'];
-let docButtonHolder = $('#buttonHolder');
-let docDisplay = $('#display');
-let userInput = $('#userInput')
-let inputBox = $('#inputBox');
-let userSubmit = $('#userSubmit');
+  let topics = ['murica', 'freedom', 'usa', 'patriot', 'america', 'donald trump', 'hillary clinton', 'barack obama'];
+  let docButtonHolder = $('#buttonHolder');
+  let docDisplay = $('#display');
+  let userInput = $('#userInput')
+  let inputBox = $('#inputBox');
+  let userSubmit = $('#userSubmit');
 
-
-console.log(topics);
-
-userSubmit.on('click', function(event){
-  console.log('submitted!');
-  userGives = inputBox.val().trim()
-  topics.push(userGives)
-  inputBox.val('');
-  generateButtons();
-
-});
-
-const generateButtons = function(){
-  $('#buttonHolder').empty();
-
-  topics.forEach(function(topic){
-
-    console.log(topic);
-    let button = $('<button>').on('click', function(){
-      docDisplay.empty();
-      let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=42Jbadnmd723gU0vIJSYwE3eeyXksAjT&limit=10&rating=g&q="+topic;
-      $.ajax({
-        url: queryUrl,
-        method: "GET"
-      }).then(function(response){
-        console.log(response);
-        response.data.forEach(img => {
-        const image = $('<img />').attr('src', img.images.original.url);
-        docDisplay.prepend(image);
-        });
-        // console.log('replied!');
-        // docDisplay.prepend($('<img>'));
-      });
-
-    });
-    docButtonHolder.append(button.text(topic));
-
+  userSubmit.on('click', function(event) {
+    userGives = inputBox.val().trim()
+    topics.push(userGives)
+    inputBox.val('');
+    generateButtons();
   });
-};
 
-generateButtons();
-
-
+  const generateButtons = function() {
+    $('#buttonHolder').empty();
+    topics.forEach(function(topic) {
+      let button = $('<button>').on('click', function() {
+        docDisplay.empty();
+        let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=42Jbadnmd723gU0vIJSYwE3eeyXksAjT&limit=10&rating=g&q=" + topic;
+        $.ajax({
+          url: queryUrl,
+          method: "GET"
+        }).then(function(response) {
+          response.data.forEach(img => {
+            const image = $('<img />').attr('src', img.images.downsized_still.url).on('click', function() {
+              var src = ($(this).attr('src') === img.images.downsized_still.url)
+               ? img.images.original.url
+               : img.images.downsized_still.url;
+              $(this).attr('src', src);
+            });
+            docDisplay.prepend(image);
+          });
+        });
+      });
+      docButtonHolder.append(button.text(topic));
+    });
+  };
+  generateButtons();
 });
