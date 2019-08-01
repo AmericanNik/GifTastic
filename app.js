6e596e9c0023a26f631ea6521b84c4ea019cf15d
AmericanNik <49841405+AmericanNik@users.noreply.github.com>
2019-08-01 03:16:47 -0400

    generate buttons, clears previous display, and on click shows 10 related GIFs

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 $(document).ready(function(){
-  console.log('ready')
+  console.log('ready');
+
+let topics = ['murica','freedom','usa','patriot','america','donald trump', 'hillary clinton','barack obama'];
+let docButtonHolder = $('#buttonHolder');
+let docDisplay = $('#display');
+
+console.log(topics);
+
+const generateButtons = function(){
+
+  topics.forEach(function(topic){
+
+    console.log(topic);
+    let button = $('<button>').on('click', function(){
+      docDisplay.empty();
+      let queryUrl = "https://api.giphy.com/v1/gifs/search?api_key=42Jbadnmd723gU0vIJSYwE3eeyXksAjT&limit=10&rating=g&q="+topic;
+      $.ajax({
+        url: queryUrl,
+        method: "GET"
+      }).then(function(response){
+        console.log(response);
+        response.data.forEach(img => {
+        const image = $('<img />').attr('src', img.images.original.url);
+        docDisplay.prepend(image);
+        });
+        // console.log('replied!');
+        // docDisplay.prepend($('<img>'));
+      });
+
+    });
+    docButtonHolder.append(button.text(topic));
+
+  });
+};
+
+generateButtons();
+
+
 });
